c4f36c7d17babfed13e9f4bef9fc46388fa149d1
Andres Aranguren <aranguren.andres9712@gmail.com>
2025-05-05 00:42:58 +0200

    preprocessing

--- levels/utils/process_data.py
+++ levels/utils/process_data.py
@@ -65,18 +65,70 @@ class ProcessData:
         print(f"Extracted {len(patches)} patches.")
         return patches
 
+class ProcessDataSymbolic:
+    def __init__(self, config_path: str = None, img_name: str = None):
+        self.config = load_config(config_path)
+        self.folder_path = self.config["data_process"]["folder_path"]
+        self.window_dim = self.config["data_process"]["sliding_window"]  # (tiles_H, tiles_W)
+        self.stride = self.config["data_process"].get("stride", 1)       # in pixels
+        self.img_name = img_name
+    
+    def load_symbolic(self):
+        # Presevers the exact format of the txt file including:
+        # The number of lines (rows)
+        # Number of characters per line (columns)
+        
+        if not os.path.exists(self.folder_path):
+            raise FileNotFoundError(f"File not found: {self.folder_path}")
+        img_path = os.path.join(self.folder_path,self.img_name)
+        with open(img_path,'r') as f:
+            lines = [line.rstrip('\n') for line in f]
+        
+        self.lines = lines
+        
+        #print the level
+        for row in lines:
+            print(row)
+        return lines
+    
+    def crop_symbolic(self):
+        win_h, win_w = self.window_dim
+        #Get full dimensions of the level
+        file_height = len(self.lines)
+        file_width = len(self.lines[0])
+        if file_height < win_h:
+            raise ValueError(f"File height {file_height} is less than window height {win_h}")
+        
+        # Slide window horizontally
+        patches = [] # store patches in a list
+        for x in range(0,file_width-win_w,self.config["data_process"]["stride"]):
+            patch = [line[x:x+win_w] for line in self.lines[:win_h]] #apply the cropping for the full height of the file
+            patches.append(patch)
+        print(f"Loaded level of size {file_height}x{file_width} characters")
+        print(f"Extracted {len(patches)} patches of size {win_h}x{win_w}")
+        self.patches = patches
+
+        return patches
+    def visualize_patches(self, n:int = 10):
+        # Visualize the patches
+        for i, patch in enumerate(self.patches[0:n]):
+            print(f"Patch {i+1}:")
+            for line in patch:
+                print(line)
+            print("\n")
+
+    
+
+
 
 
 if __name__ == "__main__":
     config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'config.yaml'))
-    processor = ProcessData(config_path=config_path, img_name="mario_1_1.png")
-    processor.read_image_data(show=False)
-    patches = processor.crop_horizontal_strips()
-    print("Displaying first 10 patches...")
-    samples = 15
-    for i, patch in enumerate(patches[:samples]):
-        plt.imshow(patch)
-        plt.axis('off')
-        plt.title(f"Patch {i+1}")
-        plt.show()
+    symb_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'symbol'))
+    symb_file = 'mario_1_1.txt' # File name
+    txt_path = os.path.join(symb_folder, symb_file)
+    processor = ProcessDataSymbolic(config_path=config_path, img_name="mario_1_1.txt") # Intialize processor class
+    symb_file = processor.load_symbolic() #Load the symbolic data
+    patches = processor.crop_symbolic() #Crop the symbolic data
+    processor.visualize_patches(10) # Visualize the first n patches
 
